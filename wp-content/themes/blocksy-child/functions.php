42b21b3fa063785b1eac0824fb19f7d70d9e621d
GQUIDU <124070071+GQUIDU@users.noreply.github.com>
2023-02-14 18:34:48 +0100

    Test modfication functions.php

--- wp-content/themes/blocksy-child/functions.php
+++ wp-content/themes/blocksy-child/functions.php
@@ -5,3 +5,7 @@ if (! defined('WP_DEBUG')) {
 add_action( 'wp_enqueue_scripts', function () {
 	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 });
+
+add_action('admin_init', function(){
+ echo 'start';
+});
