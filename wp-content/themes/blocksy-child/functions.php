99feabd388ffcfc4d21fe569d8637e6fe0828913
GQUIDU <124070071+GQUIDU@users.noreply.github.com>
2023-03-01 18:22:56 +0100

    Avancement site web

--- wp-content/themes/blocksy-child/functions.php
+++ wp-content/themes/blocksy-child/functions.php
@@ -4,4 +4,20 @@ if (! defined('WP_DEBUG')) {
 }
 add_action( 'wp_enqueue_scripts', function () {
 	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+	
+	// enqueue child styles
+	wp_enqueue_style('child-theme', get_stylesheet_directory_uri() .'/style.css', array('parent-style'));
 });
+
+// Ajout du lien Admin si l'utilisateur est connecté
+add_filter( 'wp_nav_menu_items','add_admin_link', 10, 2 );
+
+function add_admin_link( $items, $args ) {
+    if (is_user_logged_in() && $args->menu == 2) {
+
+        $items .= '<li><a href="'. get_admin_url() .'" class="ct-menu-link">Admin</a></li>';
+
+    }
+    return $items;
+
+}
